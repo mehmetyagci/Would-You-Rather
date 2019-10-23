645f1c2a1f1ef0d8412aa27bfae2b3c5f8aaf5bf
Mehmet Yağcı <mehmetyagci53@gmail.com>
2019-10-23 07:04:06 +0300

    First Reducers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
-import { Provider } from './react-redux';
+import { Provider } from 'react-redux';
 import reducer from './reducers';
 
 const store = createStore(reducer);
-
 
 ReactDOM.render(
     <Provider store={store}>
