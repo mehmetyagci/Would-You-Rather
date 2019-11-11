a5384edcd1d46563b916a1286273b1033f32c212
Mehmet Yağcı <mehmetyagci53@gmail.com>
2019-11-11 09:28:33 +0300

    Progress implemented.

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,81 +1,39 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import {handleAnswerQuestion} from '../actions/questions';
+import { handleAnswerQuestion } from "../actions/questions";
 
 class QuestionPage extends Component {
   handleAnswer = (e, answer) => {
-    e.preventDefault ();
-
-    const {dispatch, question, authedUser} = this.props;
-
-    // console.log (`Question->handleAnswer1:this.props:`);
-    // console.log (this.props);
-    // console.log (Object.keys(this.props));
-    // console.log (Object.values(this.props));
-    // console.log (`Question->handleAnswer2:question:${question} autherUser:${authedUser}`);
-
-    dispatch (
-      handleAnswerQuestion ({
+    e.preventDefault();
+    const { dispatch, question, authedUser } = this.props;
+    dispatch(
+      handleAnswerQuestion({
         id: question.id,
         answer: answer,
-        authedUser,
+        authedUser
       })
     );
   };
 
-  renderDiv2 () {
+  renderDiv(optionVoteCount, totalCount) {
+    const percentageValue = Math.round((optionVoteCount * 100) / totalCount);
     return (
-      <div className="ui progress" data-percent="19">
-        <div className="bar" style={{transition: '300ms', width: '19%'}}>
-          <div className="progress">19%</div>
-        </div>
-      </div>
-    );
-  }
-
-  renderDiv (optionVoteCount, totalCount) {
-    const percentageValue = (optionVoteCount * 100 / totalCount).toFixed (1);
-    //const stylePercentageValue = `transition: '300ms', width: '19%'`;
-
-    const stylePercentageValue = `transition: all 300ms ease 0s; width: 19%;`;
-
-    console.log ('percentageValue:', percentageValue);
-    console.log ('stylePercentageValue:', stylePercentageValue);
-
-    return (
-      <div className="ui progress" data-percent={percentageValue}>
-        <div className="bar" style={{stylePercentageValue}}>
+      <div className="ui teal progress" data-percent={percentageValue}>
+        <div className="bar" style={{ width: `${percentageValue}%` }}>
           <div className="progress">{percentageValue}%</div>
         </div>
       </div>
     );
-
-    //const percentageValue = (optionVoteCount * 100 / totalCount).toFixed (1);
-    //const stylePercentageValue = `width: '${percentageValue}%'`;
-
-    return (
-      <div>
-        <h1>{optionVoteCount}</h1>
-        <h2>{totalCount}</h2>
-        <h3>percentageValue</h3>
-
-        <div className="ui progress" data-percent={percentageValue}>
-          <div className="bar" style={{stylePercentageValue}}>
-            <div className="progress">{percentageValue}%</div>
-          </div>
-        </div>
-      </div>
-    );
   }
 
-  renderQuestion () {
-    const {authedUser, question, questionUser} = this.props;
+  renderQuestion() {
+    const { authedUser, question, questionUser } = this.props;
     const userAnswered =
-      question.optionOne.votes.includes (authedUser) ||
-      question.optionTwo.votes.includes (authedUser);
+      question.optionOne.votes.includes(authedUser) ||
+      question.optionTwo.votes.includes(authedUser);
 
-    console.log ('userAnswered:', userAnswered);
+    console.log("userAnswered:", userAnswered);
     if (userAnswered) {
       return (
         <div className="ui grid internally celled">
@@ -83,9 +41,7 @@ class QuestionPage extends Component {
             <div className="four wide floated column">
               <div className="header">{questionUser.name} asked:</div>
             </div>
-            <div className="eight wide floated column">
-              Results:
-            </div>
+            <div className="eight wide floated column">Results:</div>
           </div>
 
           <div className="row">
@@ -100,53 +56,54 @@ class QuestionPage extends Component {
             <div className="eight wide floated column">
               <div className="ui segment">
                 <h3 className="ui header">{question.optionOne.text}</h3>
-                <p>
-                  {question.optionOne.votes.length} out of{' '}
-                  {question.optionOne.votes.length +
-                    question.optionTwo.votes.length}
-                  {' '}
-                  votes{' '}
-                </p>
 
-                {this.renderDiv (
+                <span>
+                  {question.optionOne.votes.includes(authedUser) && (
+                    <div className="ui teal ribbon label">
+                      <i className="star icon" /> Your Vote
+                    </div>
+                  )}
+                </span>
+
+                {this.renderDiv(
                   question.optionOne.votes.length,
                   question.optionOne.votes.length +
                     question.optionTwo.votes.length
                 )}
 
-                {this.renderDiv2 ()}
-
-                <span>
-                  {question.optionOne.votes.includes (authedUser) &&
-                    <div className="ui teal ribbon label">
-                      <i className="star icon" /> Your Vote
-                    </div>}
-                </span>
+                <p>
+                  {question.optionOne.votes.length} out of{" "}
+                  {question.optionOne.votes.length +
+                    question.optionTwo.votes.length}{" "}
+                  votes{" "}
+                </p>
               </div>
-
               <div className="ui segment">
-
                 <h3 className="ui header">{question.optionTwo.text}</h3>
 
                 <span>
-                  {question.optionTwo.votes.includes (authedUser) &&
+                  {question.optionTwo.votes.includes(authedUser) && (
                     <div className="ui teal ribbon label">
                       <i className="star icon" /> Your Vote
-                    </div>}
+                    </div>
+                  )}
                 </span>
 
+                {this.renderDiv(
+                  question.optionTwo.votes.length,
+                  question.optionOne.votes.length +
+                    question.optionTwo.votes.length
+                )}
+
                 <p>
-                  {question.optionTwo.votes.length} out of{' '}
+                  {question.optionTwo.votes.length} out of{" "}
                   {question.optionOne.votes.length +
-                    question.optionTwo.votes.length}
-                  {' '}
-                  votes{' '}
+                    question.optionTwo.votes.length}{" "}
+                  votes{" "}
                 </p>
 
                 <div />
-
               </div>
-
             </div>
           </div>
         </div>
@@ -174,7 +131,7 @@ class QuestionPage extends Component {
 
               <button
                 className="replying-to"
-                onClick={e => this.handleAnswer (e, 'optionOne')}
+                onClick={e => this.handleAnswer(e, "optionOne")}
               >
                 Vote OptionOne
               </button>
@@ -185,7 +142,7 @@ class QuestionPage extends Component {
 
               <button
                 className="replying-to"
-                onClick={e => this.handleAnswer (e, 'optionTwo')}
+                onClick={e => this.handleAnswer(e, "optionTwo")}
               >
                 Vote OptionTwo
               </button>
@@ -196,28 +153,28 @@ class QuestionPage extends Component {
     }
   }
 
-  render () {
-    console.log ('QuestionPage->render');
-    console.log (this.props);
-    const {question} = this.props;
+  render() {
+    console.log("QuestionPage->render");
+    console.log(this.props);
+    const { question } = this.props;
 
     if (question == null) {
       // TODO: this line go to 404 page
       return <p>This Question doesn't exist</p>;
     }
-    return <div>{this.renderQuestion ()}</div>;
+    return <div>{this.renderQuestion()}</div>;
   }
 }
 
-function mapStateToProps ({authedUser, questions, users}, props) {
-  const {id} = props.match.params;
+function mapStateToProps({ authedUser, questions, users }, props) {
+  const { id } = props.match.params;
   const question = questions[id];
   const questionUser = users[question.author];
   return {
     authedUser,
     question,
-    questionUser,
+    questionUser
   };
 }
 
-export default connect (mapStateToProps) (QuestionPage);
+export default connect(mapStateToProps)(QuestionPage);
