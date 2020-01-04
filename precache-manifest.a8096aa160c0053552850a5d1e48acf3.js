55515842c416e1e8b7d9445fa89607a97e7c261d
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-04 15:12:55 +0300

    Updates

---
+++ precache-manifest.a8096aa160c0053552850a5d1e48acf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5071016a319257806ce2665070f6881",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "1a5f7259094b43cfafdc",
+    "url": "/would-you-rather/static/css/main.7839cd98.chunk.css"
+  },
+  {
+    "revision": "f68a73e6c54161e95521",
+    "url": "/would-you-rather/static/js/2.90e7db48.chunk.js"
+  },
+  {
+    "revision": "1a5f7259094b43cfafdc",
+    "url": "/would-you-rather/static/js/main.ba3eb925.chunk.js"
+  },
+  {
+    "revision": "e502d67b37ef26386f1e",
+    "url": "/would-you-rather/static/js/runtime-main.4a50dc81.js"
+  }
+]);
